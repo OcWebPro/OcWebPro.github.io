fdb475059273f01056ef64fe1821194de478ac50
OcWebPro <34339909+OcWebPro@users.noreply.github.com>
2018-09-07 02:04:27 +0300

    fgfgfg
    Verstka

---
+++ app/js/common.js
@@ -0,0 +1,14 @@
+$(function() {
+
+$('.slider').owlCarousel({
+    margin:0,
+    loop:true,
+    items:1,
+    dots: true,
+    animateOut: 'fadeOut',
+    nav: true,
+    autoplay: true,
+  	navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+});
+
+});
